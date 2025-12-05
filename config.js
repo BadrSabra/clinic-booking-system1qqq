db570eb08851c6b77a6ce73032bf0555c91e958c
BadrSabra <badraldien111@gmail.com>
2025-12-05 22:01:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
 
+const config = {
+    clinicName: "عيادة البرو",
+    clinicLogo: "assets/images/logo.png",
+    clinicAddress: "الرياض، المملكة العربية السعودية",
+    clinicPhone: "+966500000000",
+    workingHours: {
+        start: "08:00",
+        end: "20:00"
+    },
+    appointmentDuration: 30, // دقائق لكل موعد
+    currency: "ر.س",
+    language: "ar",
+    timezone: "Asia/Riyadh"
+};
+
+export default config;
